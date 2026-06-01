042f11af100ad1ecbcba2dd24f9dc71f3c10f3f5
nashalexisdolores <alxsdlrs@gmail.com>
2026-06-01 14:22:10 +0800

    instantiate car_logic in car_app; apply object composition for logic-to-UI integration

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import tkinter as tk
 from car_logic import car_logic
+
+class car_app:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Python Performance Dashboard")
+        self.root.geometry("400x350")
+        self.root.configure(bg="#1a1a1b")  
+
+        self.my_car = car_logic(2026, "Nirvana Cyber-GT")
+
+        self.setup_ui()
